27c940c7d93f2eb6522ae19d743bbd874cd38ac1
Itaitay <itais@gettaxi.com>
2018-03-13 23:33:08 +0200

    chore(lib): compile from src with `babel`

--- src/components/addons/MarkerClusterer.jsx
+++ src/components/addons/MarkerClusterer.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import MarkerClustererPlus from "marker-clusterer-plus"
+import MarkerClustererPlus from "marker-clusterer-plus/src/markerclusterer"
 
 import {
   construct,
